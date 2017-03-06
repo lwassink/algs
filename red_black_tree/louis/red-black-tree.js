72b898a68296dd3736006080b6e9ab940223096a
louisscruz <louisstephancruz@me.com>
2017-03-05 23:04:47 -0800

    Add red black tree

---
+++ red_black_tree/louis/red-black-tree.js
@@ -0,0 +1,41 @@
+import RedBlackNode from './red-black-node';
+
+class RedBlackTree {
+  constructor() {
+    this.root = null;
+  }
+
+  get(key) {
+    return this.root ? this.root.find(key) : null;
+  }
+
+  put(key, value) {
+    if (!this.root) {
+      this.root = new RedBlackNode(key, value);
+      return this.root;
+    }
+    return this.handlePut(key, value, this.root);
+  }
+
+  handlePut(key, value, node) {
+    if (node === null) return new RedBlackNode(key, value);
+    if (key < node.key) {
+      node.left = this.handlePut(key, value, node.left);
+    } else if (key > node.key) {
+      node.right = this.handlePut(key, value, node.right);
+    } else if (key === node.key) {
+      node.setValue(value);
+    }
+
+    if (node.rightIsRed() && !node.leftIsRed()) {
+      if (this.root === node) this.root = node.right;
+      node = node.rotateLeft();
+    }
+    if (node.leftIsRed() && node.leftLeftIsRed()) node = node.rotateRight();
+    if (node.leftIsRed() && node.rightIsRed()) node.flipColors();
+
+    return node;
+  }
+}
+
+export default RedBlackTree;
